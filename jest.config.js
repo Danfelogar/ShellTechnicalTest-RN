5f1dc7ef3b448b66f7fbd67da7ef43b84e55e594
Danfelogar <danypolo18@hotmail.com>
2025-10-19 12:51:17 -0500

    feat: fixing setups declarations in ci-cd.yml and jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '**/?(*.)+(spec|test).{js,jsx,ts,tsx}',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+  ],
 };
