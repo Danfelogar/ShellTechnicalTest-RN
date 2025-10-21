13617dc5dbcb1ff68b2970d22abecb4d404ec102
Danfelogar <danypolo18@hotmail.com>
2025-10-21 18:42:35 -0500

    fix: add gesture handler mock for Jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-native-vector-icons|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|@react-native-vector-icons|@react-navigation|react-native-gesture-handler|react-native-reanimated|react-native-screens|react-native-safe-area-context)/)',
   ],
   moduleNameMapper: {
     // Mock for static files
@@ -11,6 +11,8 @@ module.exports = {
     // Mock for styles files
     '@react-native-vector-icons/evil-icons': '<rootDir>/__mocks__/iconMock.js',
     '@react-native-vector-icons/ant-design': '<rootDir>/__mocks__/iconMock.js',
+    // Mock gesture handler
+    'react-native-gesture-handler': '<rootDir>/__mocks__/gestureHandlerMock.js',
   },
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/types/**'],
