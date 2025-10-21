8edf3e2a39e90d222208ba0e1a35f14dd46e44aa
Danfelogar <danypolo18@hotmail.com>
2025-10-21 18:08:05 -0500

    fix: configure Jest to mock static assets and vector icons

---
+++ __mocks__/iconMock.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MockIcon = () => React.createElement('Icon', null);
+
+export default MockIcon;
--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|@react-native-vector-icons|@react-navigation)/)',
   ],
-  testMatch: [
-    '**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '**/?(*.)+(spec|test).{js,jsx,ts,tsx}',
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/__tests__/**',
-  ],
+  moduleNameMapper: {
+    // Mock for static files
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    // Mock for styles files
+    '@react-native-vector-icons/evil-icons': '<rootDir>/__mocks__/iconMock.js',
+    '@react-native-vector-icons/ant-design': '<rootDir>/__mocks__/iconMock.js',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/types/**'],
 };
